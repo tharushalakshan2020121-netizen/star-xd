6e6c9be52dce7311fe53e832040ab505b3549e00
Malvin King <malvintech2@gmail.com>
2025-08-08 11:28:00 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,14 +1,22 @@
+require('dotenv').config();
+
 const settings = {
-  packname: 'zenthra',
-  author: '‎',
-  botName: "Zenthra-MD",
-  botOwner: '𝙼𝚊𝚕𝚟𝚒𝚗 𝙺𝚒𝚗𝚐', // Your name
-  ownerNumber: '263776388689', //Set your number here without + symbol, just add country code & number without any space
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
-  version: "0.0.1",
-  menuImageUrl: "https://files.catbox.moe/rjrgn3.jpg",
+  packname: 'sᴛᴀʀ xᴅ',
+  author: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ',
+  botName: 'sᴛᴀʀ xᴅ', //Your bot name
+  version: '1.2.0',
+  botOwner: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //Your name
+  imageUrl: 'https://i.ibb.co/rRg9wTZV/malvin-xd.jpg',    
+  ownerNumber: process.env.OWNER_NUMBER || '263714757857',
+  giphyApiKey: process.env.GIPHY_API_KEY || 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: 'public',
+  description: 'This is a bot for managing group commands and automating tasks.',
+  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || 'true', // Automatically view WhatsApp statuses
+  AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || 'true', // Automatically react to WhatsApp statuses with random emoji
+    
+  SESSION_ID: process.env.SESSION_ID || ''// make sure it starts with starecore~
 };
+
+global.SESSION_ID = settings.SESSION_ID;
 
 module.exports = settings;
